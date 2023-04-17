af77e9e73af3ee16400ed8df4b1affcf95063815
04felixli <115678315+04felixli@users.noreply.github.com>
2023-04-17 19:38:25 -0400

    Optimized previous checkCheck function
    - King cannot make a move that results in itself being in check 
    - Pinned pieces cannot move anymore 
    - checkCheck function now checks if the king is in check by opposing bishops, knights, queen, rooks, and pawns 
    - Main features still needed: checkmate, stalemate, castling, en passant, pawn promotion

--- board_class.py
+++ board_class.py
@@ -15,6 +15,7 @@ class Board:
         self.blackKingLocation = (0, 4) 
         self.whiteKingLocation = (7, 4)
         self.validMoves = [] # a list of all possible valid moves at a certain colors turn for the piece clicked on
+        self.stuckPieces = [] # a list of all pieces that if moved, will result in the king of the same color being in check
 
     # generate all possible moves for a pawn 
     def allPawnMoves(self, currentRow, currentColumn, DIMENSION):
@@ -57,8 +58,10 @@ class Board:
         # currentColumn = clicks[1] # column the knight is on before being moved
 
         # All possible vertical and horizontal movements of knight: eg.(verticalMoves[0], horizontalMoves[0])
-        verticalMoves = [-2, -2, -1, -1, 1, 1, 2, 2]
+        verticalMoves = [-2, -2, -1, -1, 1, 1, 2, 2]      
         horizontalMoves = [-1, 1, -2, 2, 2, -2, -1, 1]
+
+        # knightMoves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, 2), (1, -2), (2, -1), (2, 1)]
 
         # add up to 8 possibilities of moves for a knight
         for i in range(8):
@@ -150,8 +153,8 @@ class Board:
 
     # check if the piece in the way is the same color as the piece being moved 
     def checkSameColor(self, destinationRow, destinationColumn, currentRow, currentColumn):
-        pieceToMove = self.board[destinationRow][destinationColumn]
-        pieceToReplace = self.board[currentRow][currentColumn]
+        pieceToMove = self.board[currentRow][currentColumn]
+        pieceToReplace = self.board[destinationRow][destinationColumn]
 
         # if the pieces are different colors, the pieceToMove can replace the pieceToReplace
         if pieceToMove[0] != pieceToReplace[0]:
@@ -159,6 +162,130 @@ class Board:
         else:
             return True
     
+    # checks who's turn it is
+    def checkValidTurn(self, clicks):
+        currentRow = clicks[0] # row the piece is on before being moved
+        currentColumn = clicks[1] # column the piece is on before being moved
+        destinationRow = clicks[2]
+        destinationColumn = clicks[3]
+        pieceToMove = self.board[currentRow][currentColumn]
+        destination = self.board[destinationRow][destinationColumn] 
+        pieceMovedColor = pieceToMove[0]
+        destinationColor = destination[0]
+
+        # returns true if the piece being moved is the correct color, false otherwise
+        if ((pieceMovedColor == 'w' != destinationColor) and self.whiteTurn == True) or ((pieceMovedColor == 'b' != destinationColor) and self.whiteTurn == False):
+            return True
+        else:
+            return False
+
+    # checks if a piece can move to a certain location
+    def checkValidMove(self, clicks, DIMENSION):
+        pieceClickedOn = self.board[clicks[0]][clicks[1]][1]
+        destination = (clicks[2], clicks[3])
+
+        if pieceClickedOn not in self.stuckPieces:
+            self.generateAllValidMovesForPiece(clicks[0], clicks[1], pieceClickedOn, DIMENSION)
+            
+            if destination in self.validMoves:
+                self.validMoves.clear()
+                return True
+            else:
+                return False
+        else: 
+            return False
+        
+    # generates all valid moves for a piece at it's currentRow and currentColumn
+    def generateAllValidMovesForPiece(self, currentRow, currentColumn, piece, DIMENSION):
+        if piece == 'P':
+            self.allPawnMoves(currentRow, currentColumn, DIMENSION)
+        elif piece == 'N':
+            self.allKnightMoves(currentRow, currentColumn, DIMENSION)
+        elif piece == 'B':
+            self.allBishopMoves(currentRow, currentColumn, DIMENSION)
+        elif piece == 'R':
+            # bs.allRookMoves(clicks, DIMENSION)
+            self.allRookMoves(currentRow, currentColumn, DIMENSION)
+        elif piece == 'Q':
+            self.allRookMoves(currentRow, currentColumn, DIMENSION)
+            self.allBishopMoves(currentRow, currentColumn, DIMENSION)
+        elif piece == 'K':
+            self.allKingMoves(currentRow, currentColumn, DIMENSION)
+
+    # checks if a move made by a color results in the same color king being in check
+    # checks if a move made by the opposing color results in check 
+    def checkCheck(self, DIMENSION):
+        
+        # need to check up, down, right, left, up left, up right, down right, down left, 
+        # for knights that can check the king, just pretend king is a knight and generate all moves for it
+
+        pathsToKing = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (1, -1), (1, 1), (-1, 1), 
+                       (-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, 2), (1, -2), (2, -1), (2, 1)]
+
+        if self.whiteTurn == False: # if it's black's turn to move, need to check if black king is in check by white pieces
+            currentRow = self.blackKingLocation[0]
+            currentColumn = self.blackKingLocation[1]
+            rowOfPawnCheck = 1 # The only way for a white pawn to check the black king is if it is the row "below" the king
+        else: 
+            currentRow = self.whiteKingLocation[0]
+            currentColumn = self.whiteKingLocation[1]
+            rowOfPawnCheck = -1 # The only way for a white pawn to check the black king is if it is the row "above" the king
+
+        for path in pathsToKing:
+            for i in range(1, DIMENSION):
+
+                if path[0] == 2 or path[1] == 2 or path[0] == -2 or path[1] == -2: # look for checking knights
+                    destinationRow = currentRow + path[0]
+                    destinationColumn = currentColumn + path[1]
+                else:
+                    destinationRow = currentRow + path[0] * i 
+                    destinationColumn = currentColumn + path[1] * i
+
+                # Check if the new position is on the board
+                if 0 <= destinationRow < DIMENSION and 0 <= destinationColumn < DIMENSION:
+                    # check if there is a piece that has line of sight to the king  
+                    if self.checkPieceInPath(destinationRow, destinationColumn) == True:
+
+                        print("Piece in path")
+
+                        # check if the piece is a different color 
+                        if self.checkSameColor(destinationRow, destinationColumn, currentRow, currentColumn) == False:
+
+                            print("Piece is different color")
+
+                            pieceInWay = self.board[destinationRow][destinationColumn][1]
+                            if path[0] == 0 or path[1] == 0: # check if a rook or queen has vertical or horizontal line of sight to king
+                                if pieceInWay == 'R' or pieceInWay == 'Q':
+                                    print("Check by rook or Queen")
+                                    return True
+                                else: # if the piece in the way is not a rook or queen, move to next direction
+                                    break 
+                            elif path[0] == 2 or path[1] == 2 or path[0] == -2 or path[1] == -2:
+                                if pieceInWay == 'N':
+                                    print("check by knight")
+                                    return True
+                                else:
+                                    break
+                            else: # check if a bishop or queen or pawn has diagonal line of sight to king
+                                if pieceInWay == 'B' or pieceInWay == 'Q':
+                                    print("check by bishop or queen")
+                                    return True
+                                elif pieceInWay == 'P' and currentRow + rowOfPawnCheck == destinationRow: # check if a pawn of the opposite color is in the appropriate row to check the king
+                                    print("check by pawn")
+                                    return True
+                                else: # if the piece in the way is not a bishop or queen, move to next direction
+                                    break
+                        else: # if the piece in the way is of the same color, move in another direction
+                            break
+                else:
+                    break # stop from going off board in a direction
+        
+        return False
+                        
+                            
+                        
+
+        
 
 
 
